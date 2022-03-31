aa74d50a304d9a47ba3ec66ea5c9bafb1a303814
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-03-31 10:26:02 +0530

    homepage first commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/header/header.component';
 
 @NgModule({
   imports: [
@@ -15,7 +17,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomepageComponent,
+    HeaderComponent
   ],
   exports: [
     FooterComponent,
