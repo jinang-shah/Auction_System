f1618f06d407ac3b6c3373c07a525e1341a3991b
maheshwariyash <yash.maheshwari.sa@gmail.com>
2022-04-02 18:23:24 +0530

    initial seller-verification component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { SellerVerificationComponent } from './pages/seller-verification/seller-verification.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    SellerVerificationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
