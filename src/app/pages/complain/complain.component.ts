d9841a3e3e4babc095d0346a1ad9f377116d4d8e
Root User <root@localhost>
2022-04-13 20:05:51 +0530

    create admin service for manage-users and complain components

--- src/app/pages/complain/complain.component.ts
+++ src/app/pages/complain/complain.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/adminService/admin.service';
 
 @Component({
   selector: 'app-complain',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComplainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminservice:AdminService ) { }
+
+  
+
+  complains:any= [
+    {
+      name:"xyz",
+      date:"22/13/21",
+      status:"pending",
+      id:"22"
+
+    },
+    {
+      name:"xyz",
+      date:"22/13/21",
+      status:"solved",
+      id:"224"
+    },
+    {
+      name:"xyz",
+      date:"22/13/21",
+      status:"pending",
+      id:"2266"
+    }
+  ]
+
+ 
+
+  
 
   ngOnInit(): void {
+    this.adminservice.getComplain()
+    .subscribe((data)=>{
+      this.complains= data;
+    })
   }
+
 
 }
