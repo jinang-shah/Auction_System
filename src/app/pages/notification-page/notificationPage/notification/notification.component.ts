0fcc6bc4d0b127e7762e845ded38dfc6f53f99c3
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-04-19 19:24:55 +0530

    [wip]

--- src/app/pages/notification-page/notificationPage/notification/notification.component.ts
+++ src/app/pages/notification-page/notificationPage/notification/notification.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { NotificationService } from "src/app/services/notification/notification.service";
 
 @Component({
-  selector: 'app-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss']
+  selector: "app-notification",
+  templateUrl: "./notification.component.html",
+  styleUrls: ["./notification.component.scss"],
 })
 export class NotificationComponent implements OnInit {
+  constructor(public notificationService: NotificationService) {}
 
-  constructor() { }
-
+  @Input("item") item;
+  time: string;
+  status: string;
   ngOnInit(): void {
+    console.log(this.item);
   }
 
+  removeNotification(id: string) {
+    this.notificationService.removeNotification(id).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log("some error in deleting notification");
+      }
+    );
+  }
 }
