e857cb499ca4cda4d55808fe21a10cad4d6389b9
maheshwariyash <yash.maheshwari.sa@gmail.com>
2022-04-20 20:24:54 +0530

    user history done with API's

--- src/app/services/userhistory/userhistory.service.ts
+++ src/app/services/userhistory/userhistory.service.ts
@@ -9,11 +9,11 @@ export class UserhistoryService {
   constructor(private http: HttpClient) { }
 
 getbuyinghistory(){
-  return this.http.get('http://localhost:3000/getbuyinghistory')
+  return this.http.get('http://localhost:8000/user/buyinghistory/62600205c814c02a2d99a9f9')
 }
 
 getsellinghistory(){
-  return this.http.get('http://localhost:3000/getbuyinghistory')
+  return this.http.get('http://localhost:3000/user/sellinghistory/62600205c814c02a2d99a9f9')
 }
 
 }
