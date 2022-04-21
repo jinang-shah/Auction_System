98d2f22f0b27d0d2d418110fb12fde9bc5894b0f
Jinang Shah <jinang.shah@solutionanalysts.in>
2022-04-21 12:40:34 +0530

    coomments and socket working

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { GetProductByIdService } from 'src/app/services/get-product-by-id.service';
+import { Component, OnInit } from "@angular/core";
+import { GetProductByIdService } from "src/app/services/get-product-by-id.service";
+import { io } from "socket.io-client";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"],
 })
 export class ProductComponent implements OnInit {
-
   main_image!: string;
   isFavourite!: boolean;
   comment = "";
+  socket = io("ws://localhost:8000");
 
   user = {
-    user_id: "QGFBSFHMBD6446ADFBADWRW",
+    user_id: "625a60030ad994a0889689e2",
     isAdmin: false,
-    fav_products: ['SDYTFYGKJHGSDH'],
-  }
+    fav_products: ["SDYTFYGKJHGSDH"],
+  };
 
-  product:any = {
+  product: any = {
     // _id: "SDYTFYGKJHGSDH",
     // name: "Vintage Car",
     // description: "Lorem ipsum dolor sit amet deserunt molestiae odio ad!",
@@ -136,94 +137,125 @@ export class ProductComponent implements OnInit {
     //   { sender_id: "NJVJNWJNADKVNKJVB", name: "Yash Shah", data: "Hello", time: "3:15pm 02/08" },
     //   { sender_id: "NJVJNWJNADKVNKJVB", name: "Deep  Shah", data: "Hello", time: "3:15pm 02/08" },
     // ]
+  };
+
+  
+
+  constructor(private getProductById: GetProductByIdService)
+  {
+    console.log("socket : ",this.socket)
+    this.socket.on("connect",()=>{
+      console.log("new user connected")
+      this.socket.emit('userdata',{productId:this.product._id,userId:this.user.user_id})
+      
+      
+    })
+
+
   }
-
-
-  constructor(private getProductById: GetProductByIdService) { }
 
   // To add or remove item in user's favourite list
   addToFavourite() {
-    console.log("product id :", this.product._id)
+    console.log("product id :", this.product._id);
     if (!this.isFavourite) {
       this.user.fav_products.push(this.product._id);
-    }
-    else {
+    } else {
       const index = this.user.fav_products.indexOf(this.product._id);
       if (index > -1) {
         this.user.fav_products.splice(index, 1);
       }
     }
     this.isFavourite = !this.isFavourite;
-    // console.log(this.user)
   }
-
 
   add() {
-    this.getProductById.addToFavourite(this.user.user_id, this.product._id).subscribe((data) => {
-      console.log(data)
-    }),
+    this.getProductById
+      .addToFavourite(this.user.user_id, this.product._id)
+      .subscribe((data) => {
+        console.log(data);
+      }),
       (error) => {
-        console.log("add to fav Error :", error)
-      }
+        console.log("add to fav Error :", error);
+      };
   }
-
-
-
-
-
 
   //fun to check if product is in user's favourite list or not?
   checkIsFavourite(product_id) {
     if (this.user.fav_products.includes(product_id)) {
       return true;
     }
-    return false
+    return false;
   }
 
-  //submit comment 
+  //submit comment
   postComment() {
-
-
-
     const commentData = {
-      userId: this.user.user_id,
       timeStamp: new Date(),
       data: this.comment,
-    }
+      productId:this.product._id
+    };
 
-    this.getProductById.postComment(this.product._id, commentData)
-      .subscribe((data) => {
-        console.log(data);
-      })
+    this.socket.emit('sendComment',commentData)
+    // this.getProductById
+    //   .getProductById("625ec3baf4024dcedd774b8e")
+    //   .subscribe((data) => {
+    //     console.log("after commentx",data);
+    //     this.product = data;
+    //   });
+    
   }
 
+  ngOnInit(): void {
 
+    this.getProductById
+      .getProductById("625ec3baf4024dcedd774b8e")
+      .subscribe((data) => {
+        this.product = data;
 
+        this.main_image = this.product.images[0];
 
+          this.socket.on('receiveComment',(data)=>{
+           if(this.product._id===data.productId){
+             console.log("comment")
+             this.product.comments.push(data);
+             console.log(this.product)
+           } 
+        })
+        
+
+        
+        
+      });
+
+    this.isFavourite = this.checkIsFavourite(this.product._id);
+    // console.log("isFav :", this.isFavourite)
+  }
 
   // to change main product image
   changeImage(index) {
     this.main_image = this.product.images[index];
   }
 
-
-
-
-  ngOnInit(): void {
-    this
-    .getProductById
-    .getProductById("6255116196c147e65960161f")
-    .subscribe((data) => {
-      console.log(data)
-      this.product = data;
-      this.main_image = this.product.images[0];
-    })
-    
-    
-
-    this.isFavourite = this.checkIsFavourite(this.product._id)
-    // console.log("isFav :", this.isFavourite)
-
-    
-  }
+  
 }
+
+
+
+// this.getProductById
+//       .getProductById("6255116196c147e65960161f")
+//       .subscribe((data) => {
+//         this.product = data;
+
+//         this.main_image = this.product.images[0];
+
+//         console.log("A user connected",this.product);
+//         this.socket.emit('data',{productId:this.product._id,userId:this.user.user_id})
+
+//         this.socket.on('receiveComment',(data)=>{
+//           console.log("comment")
+//            this.product.comments.push(data);
+//            console.log(this.product)
+//         })
+        
+//       });
+//     });
