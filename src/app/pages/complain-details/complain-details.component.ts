fef5208c54a8407ab0f6a7e696960df36c46ddb1
priyank gondha <priyank.gondha@solutionanalysts.in>
2022-04-21 12:10:02 +0530

    update html and service

--- src/app/pages/complain-details/complain-details.component.ts
+++ src/app/pages/complain-details/complain-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ComplainDetailsService } from 'src/app/services/complain-details.service';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-complain-details',
@@ -9,36 +9,36 @@ import {Router} from '@angular/router'
 })
 export class ComplainDetailsComponent implements OnInit {
 
-  constructor(private complain: ComplainDetailsService,private router:Router) { }
+  constructor(private complain: ComplainDetailsService, private router: Router) { }
 
-  complainn:{}[] = []
-  item_namee:string="";
+  complainn: {}[] = []
+  item_namee: string = "";
 
 
   ngOnInit(): void {
-    this.complain.getcomplaindetails("625576afa1b124483980cb60")
-    .subscribe((data)=>{
-      console.log("data",data);
-      this.complainn.push(data);
-      console.log("com",this.complainn );
-      
-    })
+    this.complain.getcomplaindetails("6260ecc98480ba77b129a988")
+      .subscribe((data) => {
+        console.log("data", data);
+        this.complainn.push(data);
+        console.log("com", this.complainn);
+
+      })
   }
 
-  onselectfile(e){
-    if(e.target.files){
+  onselectfile(e) {
+    if (e.target.files) {
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
-      reader.onload=(event:any)=>{
+      reader.onload = (event: any) => {
         // this.url=event.target.result;
       }
     }
   }
 
-  viewitem(){
+  viewitem() {
     this.router.navigateByUrl('/add_item');
 
   }
 
-  
+
 }
