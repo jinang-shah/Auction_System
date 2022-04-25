a02f5baeca4fc93f4592385b22f0d7d4638fda6b
Surali <suralishah01@localhost>
2022-04-25 18:49:57 +0530

    changes in complain manageusers and admin

--- src/app/pages/manageusers/manageusers.component.ts
+++ src/app/pages/manageusers/manageusers.component.ts
@@ -13,27 +13,27 @@ export class ManageusersComponent implements OnInit {
   
 
   users:any= [
-    {
-      name:"xyz",
-      mail:"22/13/21",
-      number: 2345654323,
-      status:'verified',
-      id:"3456"
-    },
-    {
-      name:"xyz",
-      mail:"22/13/21",
-      number: 2345654323,
-      status:'verified',
-      id: "21"
-    },
-    {
-      name:"xyz",
-      mail:"22/13/21",
-      number: 2345654323,
-      status:'unverified',
-      id:"234123"
-    },
+    // {
+    //   name:"xyz",
+    //   email:"22/13/21",
+    //   mobile: 2345654323,
+    //   status:'verified',
+    //   id:"3456"
+    // },
+    // {
+    //   name:"xyz",
+    //   email:"22/13/21",
+    //   mobile: 2345654323,
+    //   status:'verified',
+    //   id: "21"
+    // },
+    // {
+    //   name:"xyz",
+    //   email:"22/13/21",
+    //   mobile: 2345654323,
+    //   status:'unverified',
+    //   id:"234123"
+    // },
     
   
   ]
@@ -45,8 +45,12 @@ export class ManageusersComponent implements OnInit {
   ngOnInit(): void {
     this.adminservice.getUsers()
     .subscribe((data)=>{
+      console.log(data);
+      
       this.users= data;
     })
+    console.log(this.users);
+    
   }
 
 }
