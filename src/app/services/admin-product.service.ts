7714d76b8acb7ccd4190b78fcb7afc6021e0211b
Root User <root@localhost>
2022-04-26 16:37:16 +0530

    admin-product verification don

--- src/app/services/admin-product.service.ts
+++ src/app/services/admin-product.service.ts
@@ -1,3 +1,5 @@
+// admin-product.service.ts
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -9,6 +11,22 @@ export class AdminProductService {
   constructor(private http: HttpClient) { }
 
   getProduct() {
-    return this.http.get('url')
+    return this.http.get('http://localhost:8000/product')
   }
-}
+
+  verifyProduct(productId,data){
+    
+
+    if(data.status==="Unverified"){
+      data.status= false;
+    }
+    else{
+      data.status = true;
+    }
+    console.log("data",data);
+    
+    let url = "http://localhost:8000/admin/product/verify/"+productId;
+    return this.http.patch(url,data);
+    
+  }
+} 
--- src/app/services/adminService/admin.service.ts
+++ src/app/services/adminService/admin.service.ts
@@ -15,4 +15,6 @@ export class AdminService {
   getUsers(){
     return this.http.get('url');
   }
+
+  
 }
