b554c085ddd5b0c9d60c5dfd14e1ba7e7a5f9763
maheshwariyash <yash.maheshwari.sa@gmail.com>
2022-05-03 18:23:16 +0530

    fixed registration page

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,16 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LoginService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public register(data) {
-    return this.http.post("http://localhost:8000/user/register", data)
+    return this.http.post("http://localhost:8000/user/register", data);
   }
-
-
 }
