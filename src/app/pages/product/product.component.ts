5137017232848c44a6fb42d8665264099089531c
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-05-06 15:25:42 +0530

    updated product component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -4,6 +4,7 @@ import { io } from "socket.io-client";
 import { ActivatedRoute } from "@angular/router";
 import { LoginService } from "src/app/services/homepage/login.service";
 import { LoaderService } from "src/app/services/loader.service";
+import { FavoriteService } from "src/app/services/favorite/favorite.service";
 
 @Component({
   selector: "app-product",
@@ -16,7 +17,7 @@ export class ProductComponent implements OnInit {
   comment = "";
   myLastBid = 0;
   customBid = null;
-  socket = io("ws://localhost:8000");
+  socket!: any;
 
   user: any = {};
   product: any = {};
@@ -25,47 +26,14 @@ export class ProductComponent implements OnInit {
     private getProductById: GetProductByIdService,
     private route: ActivatedRoute,
     private getUser: LoginService,
-    private loder: LoaderService
-  ) {
-    console.log("socket : ", this.socket)
-
-    this.socket.on("connect", async () => {
-      console.log("new user connected")
-      this.socket.emit('userdata', { productId: this.product._id, userId: this.user._id })
-    })
-  }
+    private loder: LoaderService,
+    private favService: FavoriteService
+  ) {}
 
   // To add or remove item in user's favourite list
   addToFavourite() {
-    console.log("product id :", this.product._id);
-    if (!this.isFavourite) {
-      this.user.fav_products.push(this.product._id);
-    } else {
-      const index = this.user.fav_products.indexOf(this.product._id);
-      if (index > -1) {
-        this.user.fav_products.splice(index, 1);
-      }
-    }
     this.isFavourite = !this.isFavourite;
-  }
-
-  add() {
-    this.getProductById
-      .addToFavourite(this.user.user_id, this.product._id)
-      .subscribe((data) => {
-        console.log(data);
-      }),
-      (error) => {
-        console.log("add to fav Error :", error);
-      };
-  }
-
-  //fun to check if product is in user's favourite list or not?
-  checkIsFavourite(product_id) {
-    if (this.user.fav_products.includes(product_id)) {
-      return true;
-    }
-    return false;
+    this.favService.toggleFavorite(this.isFavourite, this.product);
   }
 
   //submit comment
@@ -73,116 +41,109 @@ export class ProductComponent implements OnInit {
     const commentData = {
       timeStamp: new Date(),
       data: this.comment,
-      productId: this.product._id
+      productId: this.product._id,
     };
 
-    this.socket.emit('sendComment', commentData)
-    this.comment = ''
+    this.socket.emit("sendComment", commentData);
+    this.comment = "";
   }
 
   makeBid(newBid, addOn) {
-
     let bidAmount;
 
     if (addOn) {
       bidAmount = this.product.maxBid + newBid;
-    }
-    else {
+    } else {
       bidAmount = newBid;
     }
 
     if (bidAmount < this.product.basePrice) {
-      alert("Make bid >= Base Price")
+      alert("Make bid >= Base Price");
       this.customBid = null;
       return;
     }
     if (bidAmount < this.product.maxBid) {
-      alert("Make bid >= Current Bid Amount")
+      alert("Make bid >= Current Bid Amount");
       this.customBid = null;
       return;
     }
 
-
     const bidDetails = {
       timeStamp: new Date(),
       amount: bidAmount,
-      productId: this.product._id
+      productId: this.product._id,
     };
 
-    this.socket.emit('makeBid', bidDetails)
-    this.myLastBid = bidAmount
+    this.socket.emit("makeBid", bidDetails);
+    this.myLastBid = bidAmount;
     this.product.maxBid = bidAmount;
     this.customBid = null;
   }
 
   ngOnInit(): void {
-
     this.getUser.user.subscribe((user) => {
       this.user = user;
       console.log("user", user);
-    })
+    });
 
     let id;
     this.route.params.subscribe((params) => {
       console.log("id", params);
-      id = params.id
+      id = params.id;
     });
 
-    this.getProductById
-      .getProductById(id)
-      .subscribe((data) => {
-        this.product = data;
+    this.getProductById.getProductById(id).subscribe((data) => {
+      this.product = data;
 
-        let myBids = this.product.bidDetails.filter((bid) => {
-          return (bid.bidderId._id == this.user._id)
-        })
+      let myBids = this.product.bidDetails.filter((bid) => {
+        return bid.bidderId._id == this.user._id;
+      });
+      console.log("myfdi", myBids);
+      if (myBids && myBids.length != 0) {
+        myBids.sort((bid1, bid2) => {
+          return bid1.bidAmount - bid2.bidAmount;
+        });
+        this.myLastBid = myBids[myBids.length - 1].bidAmount;
+      } else {
+        this.myLastBid = 0;
+      }
 
-        if (myBids) {
-          myBids.sort((bid1, bid2) => {
-            return (bid1.bidAmount - bid2.bidAmount)
-          })
-          this.myLastBid = myBids[myBids.length - 1].bidAmount;
-        }
-        else {
-          this.myLastBid = 0;
-        }
+      console.log(data);
 
-        console.log(data)
-
-        this.main_image = this.product.images[0];
-
-        this.socket.on("connect", async () => {
-          console.log("new user connected")
-          this.socket.emit('userdata', { productId: this.product._id, userId: this.user._id })
-        })
-
-        this.socket.on('receiveComment', (data) => {
-          if (this.product._id === data.productId) {
-            console.log("comment")
-            this.product.comments.unshift(data);
-            console.log(this.product)
-          }
-        })
-
-        this.socket.on('receiveBid', (data) => {
-          if (this.product._id === data.productId) {
-            console.log(data)
-            console.log("new bid")
-            this.product.bidDetails.unshift(data);
-            this.product.maxBid = data.amount;
-            console.log(this.product)
-          }
-        })
+      this.main_image = this.product.images[0];
+      console.log("donn");
+      this.socket = io("ws://localhost:8000");
+      this.socket.on("connect", async () => {
+        console.log("new user connected");
+        this.socket.emit("userdata", {
+          productId: this.product._id,
+          userId: this.user._id,
+        });
       });
 
-    this.isFavourite = this.checkIsFavourite(this.product._id);
-    // console.log("isFav :", this.isFavourite)
+      this.socket.on("receiveComment", (data) => {
+        if (this.product._id === data.productId) {
+          console.log("comment");
+          this.product.comments.unshift(data);
+          console.log(this.product);
+        }
+      });
+
+      this.socket.on("receiveBid", (data) => {
+        if (this.product._id === data.productId) {
+          console.log(data);
+          console.log("new bid");
+          this.product.bidDetails.unshift(data);
+          this.product.maxBid = data.amount;
+          console.log(this.product);
+        }
+      });
+      this.isFavourite = this.user.favouriteProducts.includes(this.product._id);
+    });
   }
 
   // to change main product image
   changeImage(index) {
     this.main_image = this.product.images[index];
   }
-
-
 }
